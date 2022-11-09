56b0aee631df9aa948db5ce30cea7f23a24b16da
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-11-09 12:26:16 -0600

    javascript assessment practice, included indexOf manipulation

--- js/data-structure-manipulation.js
+++ js/data-structure-manipulation.js
@@ -2,13 +2,21 @@
 
 function tester(param1, param2) {
 
-    const arr1 = [6, 23, 4, 1];
+    const arr1 = [6, 23, 4, 1, 2];
 
     arr1.sort(function (a, b) {
         return a - b;
     });
 
     console.log("testing stuff: " + arr1);
+
+    let searchVal = arr1.indexOf(6);
+
+    console.log(searchVal);
+
+    arr1[searchVal] = 7;
+
+    console.log(arr1);
 }
 
 tester();
